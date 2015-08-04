53914d32b237cc22c5a814c2e175a6c975c79692
tournevis <arthur.ro92@gmail.com>
2015-08-04 16:27:27 +0200

    :fire: split :fire:

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+var myTextToSplit = document.getElementsByClassName('animText');
+for (var i = 0; i < myTextToSplit.length; i++) {
+  var characterArray = myTextToSplit[i].innerHTML.split('');
+  myTextToSplit[i].innerHTML = '';
+  for (var j = 0; j < characterArray.length; j++) {
+  	var span = document.createElement('span');
+  	span.appendChild(document.createTextNode( characterArray[j]));
+  	myTextToSplit[i].appendChild(span);
+  }
+}
+console.log('SPLITED');
