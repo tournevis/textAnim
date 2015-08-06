8d7993c2d59c37edd5f87f7ce74f88d6d187c926
tournevis <arthur.ro92@gmail.com>
2015-08-06 13:15:13 +0200

    Fix js split

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,23 +1,13 @@
 var myTextToSplit = document.getElementsByClassName('animText');
-
 for (var i = 0; i < myTextToSplit.length; i++) {
   /* Val for the win  */
-  var myTag = myTextToSplit[i].innerHTML.split(/(<([^>]+)>)/ig);
-  console.log(myTag);
-  var text = myTextToSplit[i].innerHTML;
-  myTextToSplit[i].innerHTML = '<span>' + text.split('').join('</span><span>') + '</span>';
+  var myTag = myTextToSplit[i].innerHTML.split(/(<[^>]+>)/g);
+  var html = '';
+  for (var j = 0; j < myTag.length; j++) {
+    html += ( !myTag[j].includes('<') ? myTag[j].split('').join('</span><span>') : myTag[j] );
+  }
+  myTextToSplit[i].innerHTML = '<span>' + html + '</span>';
 }
 console.log('SPLITED');
-
-
-// var tagRegex = /(<([^>]+)>)/ig ;
-// var tagTab = text.match(tagRegex);
-// if (tagTab){
-//   for (var i = 0; i < tagTab.length; i++) {
-//     tagRegex.exec(text);
-//     console.log(tagRegex.lastIndex);
-//   }
-//   console.log(tagTab + ' number of i : ' + i);
-// }
 
 /******  WIP  ******/
