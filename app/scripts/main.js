391042fba1704ef3d06b62b7652a2c71a8f17cee
tournevis <arthur.ro92@gmail.com>
2015-08-06 17:14:53 +0200

    WIP animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,14 @@
 var myTextToSplit = document.getElementsByClassName('animText');
+var myClass = 'animType2';
 for (var i = 0; i < myTextToSplit.length; i++) {
   /* Val for the win  */
   var myTag = myTextToSplit[i].innerHTML.split(/(<[^>]+>)/g);
   var html = '';
+  console.log(myTag);
   for (var j = 0; j < myTag.length; j++) {
-    html += ( !myTag[j].includes('<') ? myTag[j].split('').join('</span><span>') : myTag[j] );
+    html += ( !myTag[j].includes('<') ? myTag[j].split('').join('</span><span class=\'' + myClass + ' \' >') : myTag[j] );
   }
-  myTextToSplit[i].innerHTML = '<span>' + html + '</span>';
+  myTextToSplit[i].innerHTML = '<span class=\'' + myClass + ' \'>' + html + '</span>';
 }
 console.log('SPLITED');
 
