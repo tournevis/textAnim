07dba0aae3207cd2c9d9dce32807c9afcf25b2d6
tournevis <arthur.ro92@gmail.com>
2015-08-07 14:38:21 +0200

    0.01 Version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,22 @@
-var myTextToSplit = document.getElementsByClassName('animText');
-var myClass = 'animType2';
-for (var i = 0; i < myTextToSplit.length; i++) {
-  /* Val for the win  */
-  var myTag = myTextToSplit[i].innerHTML.split(/(<[^>]+>)/g);
-  var html = '';
-  console.log(myTag);
-  for (var j = 0; j < myTag.length; j++) {
-    html += ( !myTag[j].includes('<') ? myTag[j].split('').join('</span><span class=\'' + myClass + ' \' >') : myTag[j] );
+var animationType  = function(myClass){
+  var myTextToSplit = document.getElementsByClassName('animText');
+  //var myClass = 'animType1';
+  for (var i = 0; i < myTextToSplit.length; i++) {
+    /* Val for the win  */
+    var myTag = myTextToSplit[i].innerHTML.split(/(<[^>]+>)/g);
+    var html = '';
+    console.log(myTag);
+    for (var j = 0; j < myTag.length; j++){
+      console.log(myTag[j].split(' '));
+      html += ( !myTag[j].includes('<') && !myTag[j].includes('\n')  ? myTag[j].split('').join('</span><span class=\'' + myClass + ' \' >') : myTag[j] );
+    }
+    myTextToSplit[i].innerHTML = '<span class=\'' + myClass + ' \'>' + html + '</span>';
   }
-  myTextToSplit[i].innerHTML = '<span class=\'' + myClass + ' \'>' + html + '</span>';
+  console.log(myClass + ' Split done !');
 }
-console.log('SPLITED');
+document.addEventListener("DOMContentLoaded", function(event) {
+  animationType('animType1');
+});
+
 
 /******  WIP  ******/
